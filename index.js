169a1c744be082d549503edc65191f9f67769cdc
Mégas Vanzi <megas.vanzi@gmail.com>
2021-09-14 12:17:45 -0300

    initalize repo

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(3000);
+console.log("Server on port 3000");
