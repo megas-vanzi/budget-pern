82f619939398a8bd5f27f4674bb4e79294d4beda
Mégas Vanzi <megas.vanzi@gmail.com>
2021-10-05 12:39:16 -0300

    Integrating Semantic UI and adding Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Expense from "./components/Expense";
+import FormControl from "./components/FormControl";
 
 function App() {
   return (
     <div className="App">
+      <FormControl />
       <Expense />
       <header className="App-header">
         <p>Starting Budget App in React</p>
