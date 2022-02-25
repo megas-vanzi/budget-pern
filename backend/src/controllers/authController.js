b658d7de35bbdb8db0de38b0a4878e5addd97b38
Mégas Vanzillotta <megas.vanzi@gmail.com>
2022-02-24 23:34:15 -0300

    create auth controller

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const register = () => {};
+
+const login = () => {};
+
+const logout = () => {};
+
+module.exports = {
+  register,
+  login,
+  logout,
+};
