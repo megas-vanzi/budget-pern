a3a35908529191c3b8acff85c069e772f1c9ab83
Mégas Vanzillotta <megas.vanzi@gmail.com>
2022-02-24 23:44:22 -0300

    add login & register routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,8 @@ import Egresos from "./pages/Egresos";
 // Redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import LoginForm from "./components/forms/LoginForm";
+import RegisterForm from "./components/forms/RegisterForm";
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={LoginForm} />
+            <Route exact path="/register" component={RegisterForm} />
             <Route path="/abm" component={Abm} />
             <Route path="/ingresos" component={Ingresos} />
             <Route path="/egresos" component={Egresos} />
